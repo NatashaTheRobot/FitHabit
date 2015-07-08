ca2c8a3eae8023ea43c75896b89908b75d89834c
Natasha Murashev <nmurashev@gmail.com>
2015-07-08 05:39:43 -0700

    requesting authorization to events calendar

---
+++ FitHabit/EventManager.swift
@@ -0,0 +1,13 @@
+//
+//  EventManager.swift
+//  FitHabit
+//
+//  Created by Natasha Murashev on 7/8/15.
+//  Copyright © 2015 NatashaTheRobot. All rights reserved.
+//
+
+import UIKit
+
+class EventManager: NSObject {
+
+}
